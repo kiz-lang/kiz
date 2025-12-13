a54aa6f97c8db11be942888a30c4265b8b5e57c9
azhz1107cat <105213176+azhz1107cat@users.noreply.github.com>
2025-12-13 09:32:56 +0800

    更新 builtin_functions.cpp

--- libs/builtins/builtin_functions.cpp
+++ libs/builtins/builtin_functions.cpp
@@ -1,85 +1,91 @@
-#include "include/builtin_functions.hpp"
+namespace builtins {
 
-#include "models.hpp"
+model::Object* print(model::Object* self, const model::List* args) {
+    std::string text;
+    for (const auto* arg : args->val) {
+        text += arg->to_string() + " ";
+    }
+    std::cout << text << std::endl;
+    return new model::Nil();
+};
 
-namespace builtin {
+model::Object* input(model::Object* self, const model::List* args) {
+    const auto prompt_obj = get_one_arg(args);
+    std::cout << prompt_obj->to_string();
+    std::string result;
+    std::getline(std::cin, result);
+    return new model::String(result);
+};
 
-void register_builtin_functions() {
-    print = [](model::Object* self, const model::List* args) -> model::Object* {
-        std::string text;
-        for (const auto* arg : args->val) {
-            text += arg->to_string() + " ";
-        }
-        std::cout << text << std::endl;
-        return new model::Nil();
-    };
+model::Object* isinstance(model::Object* self, const model::List* args) {
+    if (!(args->val.size() == 2)) {
+        assert(false && "函数参数不足两个");
+    }
 
-    input = [](model::Object* self, const model::List* args) -> model::Object* {
-        const auto prompt_obj = get_one_arg(args);
-        std::cout << prompt_obj->to_string();
-        std::string result;
-        std::getline(std::cin, result);
-        return new model::String(result);
-    };
-
-    isinstance = [](model::Object* self, const model::List* args) -> model::Object* {
-        if (!(args->val.size() == 2)) {
-            assert(false && "函数参数不足两个");
-        }
-
-        const auto a = args->val[0];
-        const auto b = args->val[1];
-        return check_based_object(a, b);
+    const auto a = args->val[0];
+    const auto b = args->val[1];
+    return check_based_object(a, b);
     
-    };
+};
 
-    help = [](model::Object* self, const model::List* args) -> model::Object* {
+model::Object* help(model::Object* self, const model::List* args) {
+    // todo
+    return new Nil();
+};
 
-    };
+model::Object* breakpointer(model::Object* self, const model::List* args) {
+    // todo
+    return new Nil();
+};
 
-    breakpointer = [](model::Object* self, const model::List* args) -> model::Object* {
+model::Object* range(model::Object* self, const model::List* args) {
+    // todo
+    return new Nil();
+};
 
-    };
+model::Object* cmd(model::Object* self, const model::List* args) {
+    // todo
+    return new Nil();
+};
 
-    range = [](model::Object* self, const model::List* args) -> model::Object* {
+model::Object* now(model::Object* self, const model::List* args) {
+    // todo
+    return new Nil();
+};
 
-    };
+model::Object* setattr(model::Object* self, const model::List* args) {
+    // todo
+    return new Nil();
+};
 
-    cmd = [](model::Object* self, const model::List* args) -> model::Object* {
+model::Object* getattr(model::Object* self, const model::List* args) {
+    // todo
+    return new Nil();
+};
 
-    };
+model::Object* delattr(model::Object* self, const model::List* args) {
+    // todo
+    return new Nil();
+};
 
-    now = [](model::Object* self, const model::List* args) -> model::Object* {
+model::Object* getrefc(model::Object* self, const model::List* args) {
+    // todo
+    return new Nil();
+};
 
-    };
+model::Object* copy(model::Object* self, const model::List* args) {
+    // todo
+    return new Nil();
+};
 
-    setattr = [](model::Object* self, const model::List* args) -> model::Object* {
+model::Object* create(model::Object* self, const model::List* args) {
+    // todo
+    return new Nil();
+};
 
-    };
-
-    getattr = [](model::Object* self, const model::List* args) -> model::Object* {
-
-    };
-
-    delattr = [](model::Object* self, const model::List* args) -> model::Object* {
-
-    };
-
-    getrefc = [](model::Object* self, const model::List* args) -> model::Object* {
-
-    };
-
-    copy = [](model::Object* self, const model::List* args) -> model::Object* {
-
-    };
-
-    create = [](model::Object* self, const model::List* args) -> model::Object* {
-
-    };
-
-    typeofobj = [](model::Object* self, const model::List* args) -> model::Object* {
-
-    };
-}
+model::Object* typeofobj(model::Object* self, const model::List* args) {
+    // todo
+    return new Nil();
+};
 
 }
