9033728c22d10b85597bbfa95c07dc5cbe3e133e
azhz1107cat <105213176+azhz1107cat@users.noreply.github.com>
2025-12-13 10:14:38 +0800

    补充注释

--- libs/builtins/nil_obj.cpp
+++ libs/builtins/nil_obj.cpp
@@ -2,7 +2,7 @@
 
 namespace model {
 
-// Nil 相等判断：仅当另一个对象也是Nil时返回true
+// Nil.__eq__ 相等判断：仅当另一个对象也是Nil时返回true
 model::Object* nil_eq(model::Object* self, const model::List* args) {
     DEBUG_OUTPUT("You given " + std::to_string(args->val.size()) + " arguments (nil_eq)");
     assert(args->val.size() == 1 && "function Nil.eq need 1 arg");
