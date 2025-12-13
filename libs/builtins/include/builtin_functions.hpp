b067753c51e05ef9831085cf06b671071ff78afb
azhz1107cat <105213176+azhz1107cat@users.noreply.github.com>
2025-12-13 09:29:19 +0800

    更新 builtin_functions.hpp

--- libs/builtins/include/builtin_functions.hpp
+++ libs/builtins/include/builtin_functions.hpp
@@ -38,38 +38,22 @@ inline model::Object* check_based_object(model::Object* src_obj, model::Object* 
     return check_based_object_inner(src_obj, for_check_obj, visited);
 }
 
-void register_builtin_functions();
+// 内置函数
+model::Object* print(model::Object* self, const model::List* args);
+model::Object* input(model::Object* self, const model::List* args);
+model::Object* isinstance(model::Object* self, const model::List* args);
+model::Object* help(model::Object* self, const model::List* args);
+model::Object* breakpointer(model::Object* self, const model::List* args);
+model::Object* range(model::Object* self, const model::List* args);
+model::Object* cmd(model::Object* self, const model::List* args);
+model::Object* now(model::Object* self, const model::List* args);
+model::Object* setattr(model::Object* self, const model::List* args);
+model::Object* getattr(model::Object* self, const model::List* args);
+model::Object* delattr(model::Object* self, const model::List* args);
+model::Object* getrefc(model::Object* self, const model::List* args);
+model::Object* copy(model::Object* self, const model::List* args);
+model::Object* create(model::Object* self, const model::List* args);
+model::Object* typeofobj(model::Object* self, const model::List* args);
 
-using NativeFuncPtr = std::function<model::Object*(model::Object*, const model::List*)>;
-
-inline NativeFuncPtr print;
-
-inline NativeFuncPtr input;
-
-inline NativeFuncPtr isinstance;
-
-inline NativeFuncPtr help;
-
-inline NativeFuncPtr breakpointer;
-
-inline NativeFuncPtr range;
-
-inline NativeFuncPtr cmd;
-
-inline NativeFuncPtr now;
-
-inline NativeFuncPtr setattr;
-
-inline NativeFuncPtr getattr;
-
-inline NativeFuncPtr delattr;
-
-inline NativeFuncPtr getrefc;
-
-inline NativeFuncPtr copy;
-
-inline NativeFuncPtr create;
-
-inline NativeFuncPtr typeofobj;
 
 }
