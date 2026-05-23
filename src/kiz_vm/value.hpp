909c910ce6cf307843ed7924ab3b7e34530dcc62
azhz <105213176+azhz1107cat@users.noreply.github.com>
2026-05-23 15:26:42 +0800

    feat: improve value.hpp

--- src/kiz_vm/value.hpp
+++ src/kiz_vm/value.hpp
@@ -10,7 +10,27 @@ enum class ValueKind: uint8_t {
 struct Value {
     ValueKind tag;
     uint64_t data;
-        auto is_int() const noexcept -> bool {
+
+    static auto from_int(uint64_t val) noexcept -> Value {
+        return {ValueKind::Int, val};
+    }
+    static auto from_decimal(double val) noexcept -> Value {
+        Value res{};
+        res.tag = ValueKind::Decimal;
+        *reinterpret_cast<double*>(&res.data) = val;
+        return res;
+    }
+    static auto from_object(Object* obj) noexcept -> Value {
+        return {ValueKind::ObjectPtr, reinterpret_cast<uint64_t>(obj)};
+    }
+    static auto from_func(uint64_t addr) noexcept -> Value {
+        return {ValueKind::FunctionPtr, addr};
+    }
+    static auto from_string(const char* str) noexcept -> Value {
+        return {ValueKind::String, reinterpret_cast<uint64_t>(str)};
+    }
+
+    auto is_int() const noexcept -> bool {
         return tag == ValueKind::Int;
     }
     auto is_decimal() const noexcept -> bool {
@@ -32,7 +52,7 @@ struct Value {
     }
     auto as_decimal() const -> double {
         $Assert(is_decimal());
-        return reinterpret_cast<float>(data);
+        return *reinterpret_cast<const double*>(&data);
     }
     auto as_objectptr() const -> Ptr<Object> {
         $Assert(is_objectptr());
