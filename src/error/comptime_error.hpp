6b0cefe3dcd4e433e212adc0e9b203277e417419
azhz <105213176+azhz1107cat@users.noreply.github.com>
2026-05-23 14:20:43 +0800

    feat: update comptime_error.hpp

--- src/error/comptime_error.hpp
+++ src/error/comptime_error.hpp
@@ -1,7 +1,21 @@
 #pragma once
 
+#define $Err(x) x,
+enum class ComptimeErrorKind: uint8_t {
+    #include "comptime_error_kind.def"
+};
+#undef $Err
+
+#define $Err(x) case x: return Str(#x);
+inline auto comptime_error_kind_to_string(ComptimeErrorKind k) -> Str {
+    switch(k) {
+    #include "comptime_error_kind.def"
+    }
+}
+#undef $Err
+
 struct ComptimeError {
+    Span span;
     Str msg;
     ComptimeErrorKind kind;
-    Span span;
 };
