896b93b07e7b5d723d0f2c9a69e05ee02977b81b
azhz <105213176+azhz1107cat@users.noreply.github.com>
2026-05-23 14:25:31 +0800

    feat: improve lexer.hpp

--- src/kiz_compiler/lexer.hpp
+++ src/kiz_compiler/lexer.hpp
@@ -5,11 +5,14 @@ namespace kizc {
 enum class TokenKind: uint8_t {
     OpAdd, OpSub, OpMul, OpDiv, OpMod, OpEq,
     OpNe, OpGt, OpLt, OpLe, OpGe,
-    KwAnd, KwOr, KwNot, KwIf, KwElif, KwElse,
-    KwWhile, KwLoop, KwBreak, KwReturn, KwNext,
-    KwType, KwWith, KwImpl, KwAbstract, KwMove,
-    KwRef, KwMutRef, kwLet, KwPub, KwUse, KwMod,
-    KwFun, KwTry, KwWhen, KwOn,
+    KwAnd, KwOr, KwNot, 
+    KwIf, KwElif, KwElse,
+    KwWhile, KwLoop, 
+    KwBreak, KwReturn, KwNext,
+    KwType, KwWith, KwImpl, KwAbstract,
+    KwMove, KwRef, KwMutRef, KwClone,
+    kwLet, KwPub, KwUse, KwMod,
+    KwFun, KwTry, KwWhen,
     At, LParen, RParen, LBrace, RBrace, LBracket, RBracket,
     Comma, Semi, Colon, Dot, FatArrow, ThinArrow, Assign,
     Ident, String, Int, Decimal, Eof, Invaild
@@ -22,14 +25,14 @@ struct Token {
 };
 
 class Lexer {
-    Str text;
-    uint32_t pos;
-    uint32_t lineno;
-    uint8_t col;
-    Token next_cache;
+    Str text_;
+    uint32_t pos_;
+    uint32_t lineno_;
+    uint8_t col_;
+    Token next_cache_;
 public:
     Lexer(Str s)
-        : text(s), pos(0), lineno(1), col(1) {}
+        : text_(s), pos_(0), lineno_(1), col_(1) {}
     /// next
     auto next() -> Token;
     auto peek() -> Token;
