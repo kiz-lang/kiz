21fc44a41666f185b51a5a7f3b609893770805ba
azhz <105213176+azhz1107cat@users.noreply.github.com>
2026-05-31 00:11:31 +0800

    fix: small fix

--- src/kiz_vm/object.hpp
+++ src/kiz_vm/object.hpp
@@ -3,7 +3,8 @@
 struct Object {
     Ptr<Value> fields;
     uint32_t field_count
+
     uint32_t tyid;
-    uint32_t refcount;
-    bool is_moved;
+    uint32_t refcount: 31;
+    bool is_moved: 1;
 };
