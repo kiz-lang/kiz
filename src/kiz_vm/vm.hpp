1bb9bc362616fa23e3c981c5e9d6aeab8b8ae408
azhz <105213176+azhz1107cat@users.noreply.github.com>
2026-05-30 17:42:30 +0800

    feat: prepare vm for AE

--- src/kiz_vm/vm.hpp
+++ src/kiz_vm/vm.hpp
@@ -1,9 +1,32 @@
 #pragma once
 
+
+struct Segment {
+    Value* mem_start;
+    Value* mem_end;
+    uint32_t bind_handler;
+    uint32_t pred;
+    uint32_t succ;
+    uint32_t pc;
+};
+
+
 class Vm {
+    /*
+    Segment stack_;
+    Segment tmp_continuation_;
+    */
     Ptr<IRModule> irmodule_;
     JitCompiler& jitc_;
-    Vec<Value> stack_;
+    Vec<Value> static_mem_;
+    Vec<Value> stack_mem_;
+
+    /*
+    MemoryPool<Value> stack_mem_pool;
+    Vec<Segment> segment_pool;
+    uint32_t current_segement_;
+    uint32_t effect_id;
+    */
     uint32_t pc_ = 0;
 
     auto exec_unit() noexcept -> void;
@@ -11,14 +34,11 @@ class Vm {
     auto new_frame() noexcept -> void;
     auto destory_frame() noexcept -> void;
 
-    auto jit_compile() -> bool;
-    auto run_jit_code() -> Value;
+    auto jit_compile() noexcept -> RuntimeError;
+    auto run_jit_code() noexcept -> void;
     auto refresh_jit() -> void;
     auto is_jit_ready() const noexcept -> bool;
 
-
-    [[noreturn]]
-    auto diagnose() noexcept -> void;
 
 public:
     explicit Vm(Ptr<IRModule> irmod, JitCompiler& jitc)
