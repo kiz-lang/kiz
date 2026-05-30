b09d937a8f5d9bbbdee4b64d01abcc8dad7b576a
azhz <105213176+azhz1107cat@users.noreply.github.com>
2026-05-30 20:12:27 +0800

    feat: update vm.hpp

--- src/kiz_vm/vm.hpp
+++ src/kiz_vm/vm.hpp
@@ -1,34 +1,14 @@
 #pragma once
 
 
-struct Segment {
-    Value* mem_start;
-    Value* mem_end;
-    uint32_t bind_handler;
-    uint32_t pred;
-    uint32_t succ;
-    uint32_t pc;
-};
-
-
-class Vm {
-    /*
-    Segment stack_;
-    Segment tmp_continuation_;
-    */
+class Vm : NoCopyMove {
+    Stack stack_;
     Object gc_root_;
     GarbageCollection& gc_;
     JitCompiler& jitc_;
     Ptr<IRModule> irmodule_;
     Vec<Value> static_mem_;
-    Vec<Value> stack_mem_;
 
-    /*
-    MemoryPool<Value> stack_mem_pool_;
-    Vec<Segment> segment_pool_;
-    uint32_t current_segement_;
-    uint32_t current_effect_id;
-    */
     uint32_t pc_ = 0;
 
     auto exec_unit() noexcept -> void;
@@ -45,11 +25,6 @@ class Vm {
 public:
     explicit Vm(Ptr<IRModule> irmod, JitCompiler& jitc)
     : irmodule_(irmod), jitc_(jitc) {}
-
-    Vm(const Vm&) = delete;
-    Vm(Vm&&) = delete;
-    Vm& operator=(const Vm&) = delete;
-    Vm& operator=(Vm&&) = delete;
 
     auto run() noexcept -> void;
 };
