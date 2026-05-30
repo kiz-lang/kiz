a3e618270c48b1b891660f6517ffcc97913c1b80
azhz <105213176+azhz1107cat@users.noreply.github.com>
2026-05-31 00:20:43 +0800

    feat: update object.hpp

--- src/kiz_vm/object.hpp
+++ src/kiz_vm/object.hpp
@@ -3,8 +3,11 @@
 struct Object {
     Ptr<Value> fields;
     uint32_t field_count
+    uint32_t refcount;
+    bool is_moved;
+};
 
+struct GenericObject {
+    Ptr<Object> obj;
     uint32_t tyid;
-    uint32_t refcount: 31;
-    bool is_moved: 1;
 };
