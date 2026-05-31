69dac125ded2719c33437ec77f81bbd3ccb760af
azhz <105213176+azhz1107cat@users.noreply.github.com>
2026-05-31 09:05:54 +0800

    feat: create source_manager.hpp

--- src/kiz_source/source_manager.hpp
+++ src/kiz_source/source_manager.hpp
@@ -1,19 +1,10 @@
 #pragma once
 
-struct FileData {
-    Vec<uint32_t> linenomap; // index => offset
-    Str name;
-    Str path;
-    Str content;
-    uint32_t id;
-    uint32_t version = 1;
-};
 
-class SourceManager {
-    Vec<FileData> files_;
+class SourceManager : NoCopyMove {
+    Vec<FileMetaData> files_;
 
 public:
-    SourceManager() = default;
     auto get_id_by_path(Str path) const noexcept -> Option<uint32_t>;
     auto get_content_by_path(Str name) const noexcept -> Option<Str>;
     auto get_content(uint32_t file_id) const noexcept -> Option<Str>;
