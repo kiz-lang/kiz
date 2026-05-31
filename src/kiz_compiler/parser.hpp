e604337deefa04f6c6ca84f0da7b7616dd00e82d
azhz <105213176+azhz1107cat@users.noreply.github.com>
2026-05-31 08:43:16 +0800

    feat: update parser.hpp

--- src/kiz_compiler/parser.hpp
+++ src/kiz_compiler/parser.hpp
@@ -3,40 +3,44 @@
 namespace kizc {
 
 class Parser {
-    Lexer& lexer_;
-    Codegen& codegen_;
-    IRModule& irmodule_;
     Vec<Str> import_paths_;
     Vec<SymbolTable> define_stack_;
 
-    /// parse expr
-    auto parse_expr() -> void;
+    Vec<FunctionSymbol> global_function_symbols_;
+    Vec<AbstractSymbol> global_abstract_symbols_;
+    Vec<TypeSymbol> global_type_symbols_;
 
-    /// parse stmt
-    auto parse_stmt() -> void;
-    auto parse_if() -> void;
-    auto parse_loop() -> void;
-    auto parse_fn() -> void;
-    auto parse_typedef() -> void;
+    Lexer& lexer_;
+    Codegen& codegen_;
+    IRModule& irmodule_;
+
+    /// parser
+    auto parse_expr() -> ComptimeError;
+    auto parse_stmt() -> ComptimeError;
+    auto parse_if() -> ComptimeError;
+    auto parse_loop() -> ComptimeError;
+    auto parse_fn() -> ComptimeError;
+    auto parse_typedef() -> ComptimeError;
+    auto parse_abstract() -> ComptimeError;
+    auto parse_handler() -> ComptimeError;
 
     /// sema
-    auto lookup_var(Str name) -> Symbol;
-    auto lookup_fn(Str name) -> Type;
-    auto lookup_type(Str name) -> Type;
-    auto define_var(Symbol sym) -> void;
-    auto define_fn(Symbol sym) -> void;
-    auto define_type(Type sym) -> void;
+    auto lookup_var(Str name) -> uint32_t;
+    auto lookup_fn(Str name) -> uint32_t;
+    auto lookup_type(Str name) -> uint32_t;
+    auto lookup_abstract(Str name) -> uint32_t;
+    auto lookup_field(uint32_t typeid) -> uint32_t;
+    auto lookup_method(uint32_t typeid) -> uint32_t;
 
-    auto check_call(Symbol fn, Vec<Symbol> agrv) -> bool;
-    auto analyze_binary(Type l, Type r) -> Result<Type, ParserError>;
+    auto check_call(Symbol fn, Vec<uint32_t> argv) -> bool;
+    auto analyze_binary(Type l, Type r) -> Result<Type, ComptimeError>;
     auto analyze_unary(Type l, Type r) -> Result<Type, ParserError>;
 
 public:
     Parser(Vec<Str> import_paths, Lexer& l, Codegen& cg)
     : import_paths_(import_paths), lexer_(l), codegen_(cg), define_stack_({}), irmodule_({}) {}
     
-    /// parse
-    auto parse(Str txt) -> IRModule;
+    auto parse(Str txt) -> Result<IRModule, ComptimeError>;
 }
 
 }
